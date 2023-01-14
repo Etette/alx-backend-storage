e52b1baf7fa893f900327f249b7d55813949991c
Etette Etok <emmanueletok5@gmail.com>
2023-01-14 16:51:33 +0000

    fixed typo

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -4,4 +4,4 @@
 DROP VIEW IF EXISTS need_meeting;
 CREATE VIEW need_meeting AS
 SELECT name FROM students WHERE score < 80
-AND (last_meeting IS NULL OR last_meeting < DATE(CUEDATE() - INTERVAL 1 MONTH));
+AND (last_meeting IS NULL OR last_meeting < DATE(CURRENT_DATE() - INTERVAL 1 MONTH));
