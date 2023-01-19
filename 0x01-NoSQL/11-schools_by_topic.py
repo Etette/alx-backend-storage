804b8786d1ebfab5a7c244e335b2c0bee3077392
Etette Etok <emmanueletok5@gmail.com>
2023-01-19 11:24:09 +0000

    11-schools_by_topic.py

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -6,6 +6,6 @@ a specific topic
 import pymongo
 
 
-def schools_by_toopic(mongo_collection, topic):
+def schools_by_topic(mongo_collection, topic):
     """Find by topic"""
-    return mongo_collection.find({"topics": topic})
+    return mongo_collection.find({"topics":  {"$in": [topic]}})
